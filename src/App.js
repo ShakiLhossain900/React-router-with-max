c4f911ef1b5b48a0d369a51c5973dcf2fac809fb
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-06 09:15:44 +0600

    adding the reoute

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import {Route} from 'react-router-dom'
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+        <Route path="/welcome"/>
     </div>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const welcome = () => {
+    return (
+        <div>
+            <h1>The Welcoome Page</h1>
+        </div>
+    );
+};
+
+export default welcome;
