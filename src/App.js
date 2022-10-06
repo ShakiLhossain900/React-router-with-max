76d3d33173de97a810f97155a8d77934220c99aa
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-06 20:41:01 +0600

    adding dynamic route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Welcome from "./pages/Welcome";
 import Producsts from "./pages/Producsts";
 import React from "react";
 import MainHeader from "./components/MainHeader";
-import ProductDetail from './pages/ProductDetail';
+import ProductDetail from "./pages/ProductDetail";
 function App() {
   return (
     <React.Fragment>
@@ -12,7 +12,10 @@ function App() {
         <Routes>
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/products" element={<Producsts />} />
-          <Route path="/product-detail/:productId" element={<ProductDetail/>}/>   
+          <Route
+            path="/product-detail/:productId"
+            element={<ProductDetail />}
+          />
         </Routes>
       </main>
     </React.Fragment>
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import {useParams} from 'react-router-dom';
 const ProductDetail = () => {
+    const params = useParams();
+    console.log(params.productId);
     return (
         <section>
             <h1>Product Details</h1>
+            <p>{params.productId}</p>
         </section>
     );
 };
