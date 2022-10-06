6ec38d2815ddec6342cb60fe8fd38a9f6f91ddf7
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-06 09:45:30 +0600

    THE ADDED NEW INDEX CHANGES

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter } from "react-router-dom";
+
+// import "./index.css";
+// import App from "./App";
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+
+
+
+
+//CHANGE TO REACT 18
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
