a849a222203134eadaa4fd531538f16326de11d4
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-07 12:49:21 +0600

    adding the products

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route,Routes } from 'react-router-dom';
 
 const welcome = () => {
     return (
         <div>
-            <h1>The Welcoome Page</h1>
+            <h1>The Welcome Page</h1>
+            <Routes>
+                <Route path="/welcome/new-user">
+                 Welcome, new User!
+                </Route>
+            </Routes>
         </div>
     );
 };
