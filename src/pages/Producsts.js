71a90ce0ed70dae72035a60a5acb423ab74462bf
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-07 12:29:01 +0600

    adding the products

--- src/pages/Producsts.js
+++ src/pages/Producsts.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Producsts = () => {
   return (
     <div>
       <h1>The Producsts Page</h1>
-       <ul>
-          <li>A Books</li>
-          <li>Karpet</li>
-          <li>Online courses</li>
-       </ul>
+      <ul>
+        <li>
+          <Link to="/producsts/p1">A Books</Link>
+        </li>
+        <li>
+          <Link to="/producsts/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/producsts/p3">An Online Course</Link>
+        </li>
+      </ul>
     </div>
   );
 };
