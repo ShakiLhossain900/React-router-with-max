379cc5b8d0ab8635313e59f2e4220e631be82ecf
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-11 00:53:53 +0600

    adding the Link in proudct.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,13 +5,13 @@ const Products = () => {
       <h1>The Products Page</h1>
       <ul>
         <li>
-          <Link to="products/p1">A Book</Link>
+          <Link to="/products/p1">A Book</Link>
         </li>
         <li>
-          <Link to="products/p2">A Carpet</Link>
+          <Link to="/products/p2">A Carpet</Link>
         </li>
         <li>
-          <Link to="products/p3">An oline courses</Link>
+          <Link to="/products/p3">An oline courses</Link>
         </li>
       </ul>
     </section>
