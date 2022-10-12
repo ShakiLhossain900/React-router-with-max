1d57576fa1acdfcacba1324d0e4ada942b9e5fd7
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-12 14:36:29 +0600

    add commit

--- src/App.js
+++ src/App.js
@@ -36,3 +36,14 @@ export default App;
 // our-domain.com/welcome => Welcome Component
 // our-domain.com/products => Products Component
 // our-domain.com/product-detail/a-book
+
+
+
+
+//the comment myself 
+//module 011 is finished
+//this month i need fully completed the react.js
+//after one month the planed need to be changed about the react.js
+// within this year i need to be mern stack developer
+//than need to get one good job
+//with this also i have to belive the world for me as well
